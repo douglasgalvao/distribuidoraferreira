034ea95e83eea78456740e028f14e608768ddfc1
douglas <douglasgmachado98@gmail.com>
2024-08-11 18:00:13 -0300

    add: fix the search in products

--- frontend/src/app/modules/produtos/table-pagination-produto/table-pagination-produto.component.ts
+++ frontend/src/app/modules/produtos/table-pagination-produto/table-pagination-produto.component.ts
@@ -107,24 +107,44 @@ export class TableProdutosCategoriasComponent implements OnInit, AfterViewInit {
   //   };
   // }
 
+  // normalizeAccents(input: string): string {
+  //   const accentsMap: { [key: string]: string } = {
+  //     'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u',
+  //     'à': 'a', 'è': 'e', 'ì': 'i', 'ò': 'o', 'ù': 'u',
+  //     'ã': 'a', 'õ': 'o',
+  //     'â': 'a', 'ê': 'e', 'î': 'i', 'ô': 'o', 'û': 'u',
+  //     'ä': 'a', 'ë': 'e', 'ï': 'i', 'ö': 'o', 'ü': 'u'
+  //   };
+
+  //   return input.replace(/[áéíóúàèìòùãõâêîôûäëïöü]/g, match => accentsMap[match] || match);
+  // }
+
+
+  // applyFilter(event: Event) {
+  //   const filterValue = (event.target as HTMLInputElement).value;
+  //   const normalizedFilter = this.normalizeAccents(filterValue.trim().toLowerCase());
+  //   this.dataSource.filter = normalizedFilter;
+  // }
+
+
   normalizeAccents(input: string): string {
-    const accentsMap: { [key: string]: string } = {
-      'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u',
-      'à': 'a', 'è': 'e', 'ì': 'i', 'ò': 'o', 'ù': 'u',
-      'ã': 'a', 'õ': 'o',
-      'â': 'a', 'ê': 'e', 'î': 'i', 'ô': 'o', 'û': 'u',
-      'ä': 'a', 'ë': 'e', 'ï': 'i', 'ö': 'o', 'ü': 'u'
-    };
-
-    return input.replace(/[áéíóúàèìòùãõâêîôûäëïöü]/g, match => accentsMap[match] || match);
+    // Normaliza a string para remover diacríticos (acentos).
+    return input.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
   }
-
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     const normalizedFilter = this.normalizeAccents(filterValue.trim().toLowerCase());
     this.dataSource.filter = normalizedFilter;
+
+    this.dataSource.filterPredicate = (data: ProdutoElementRequest, filter: string) => {
+      // Normaliza e converte todos os valores da linha em uma única string.
+      const dataStr = this.normalizeAccents(Object.values(data).join(' ').toLowerCase());
+      // Verifica se a string normalizada dos dados inclui o filtro.
+      return dataStr.includes(filter);
+    };
   }
+
 
 
   updateTableByProdutos(produtos: ProdutoElement[]) {
