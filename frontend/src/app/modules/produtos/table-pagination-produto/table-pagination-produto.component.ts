23fb3927a2c24b39898e9f54a149bf0cef9ab4ad
douglas <douglasgmachado98@gmail.com>
2024-08-11 17:42:34 -0300

    add: images e try to fix the search in products

--- frontend/src/app/modules/produtos/table-pagination-produto/table-pagination-produto.component.ts
+++ frontend/src/app/modules/produtos/table-pagination-produto/table-pagination-produto.component.ts
@@ -95,29 +95,43 @@ export class TableProdutosCategoriasComponent implements OnInit, AfterViewInit {
     return formatter.format(valor);
   }
 
+  // applyFilter(event: Event) {
+  //   const filterValue = (event.target as HTMLInputElement).value.toLowerCase();
+  //   const normalizedFilter = this.normalizeAccents(filterValue);
+
+  //   this.dataSource.filter = normalizedFilter;
+
+  //   this.dataSource.filterPredicate = (data: ProdutoElementRequest, filter: string) => {
+  //     const dataStr = this.normalizeAccents(Object.values(data).join(' ').toLowerCase());
+  //     return dataStr.includes(filter);
+  //   };
+  // }
+
+  // normalizeAccents(input: string): string {
+  //   const accentsMap: { [key: string]: string } = {
+  //     'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u',
+  //     'à': 'a', 'è': 'e', 'ì': 'i', 'ò': 'o', 'ù': 'u',
+  //     'ã': 'a', 'õ': 'o',
+  //     'â': 'a', 'ê': 'e', 'î': 'i', 'ô': 'o', 'û': 'u',
+  //     'ä': 'a', 'ë': 'e', 'ï': 'i', 'ö': 'o', 'ü': 'u'
+  //   };
+
+  //   return input.replace(/[áéíóúàèìòùãõâêîôûäëïöü]/g, match => accentsMap[match] || match);
+  // }
+
+
   applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value.toLowerCase();
-    const normalizedFilter = this.normalizeAccents(filterValue);
-
+    const filterValue = (event.target as HTMLInputElement).value;
+    const normalizedFilter = this.normalizeAccents(filterValue.trim().toLowerCase());
     this.dataSource.filter = normalizedFilter;
-
-    this.dataSource.filterPredicate = (data: ProdutoElementRequest, filter: string) => {
-      const dataStr = this.normalizeAccents(Object.values(data).join(' ').toLowerCase());
-      return dataStr.includes(filter);
-    };
   }
 
   normalizeAccents(input: string): string {
-    const accentsMap: { [key: string]: string } = {
-      'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u',
-      'à': 'a', 'è': 'e', 'ì': 'i', 'ò': 'o', 'ù': 'u',
-      'ã': 'a', 'õ': 'o',
-      'â': 'a', 'ê': 'e', 'î': 'i', 'ô': 'o', 'û': 'u',
-      'ä': 'a', 'ë': 'e', 'ï': 'i', 'ö': 'o', 'ü': 'u'
-    };
-
-    return input.replace(/[áéíóúàèìòùãõâêîôûäëïöü]/g, match => accentsMap[match] || match);
+    return input.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
   }
+
+
+
 
 
 
