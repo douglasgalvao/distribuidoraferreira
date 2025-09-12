199af14ab3af21394ee1a383953eb19f3033ec9e
Douglas Machado <douglasgmachado98@gmail.com>
2025-09-12 16:07:11 -0300

    fix: update apiUrl to correct Heroku endpoint

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     // apiUrl: 'http://localhost:8080'
-    apiUrl: 'https://distribuidoraferreira-back.onrender.com'
+    apiUrl: 'https://distribuidorabebidas-323b6f9478c3.herokuapp.com'
 };
